88191dbec74be0207ce4030f739a572bd8de65d5
Dmitry-fq <frizhikini.d@gmail.com>
2025-07-04 11:29:32 +0500

    add other services in config

--- rococo-e2e-tests/src/test/java/ru/rococo/config/Config.java
+++ rococo-e2e-tests/src/test/java/ru/rococo/config/Config.java
@@ -21,5 +21,64 @@ public interface Config {
     String userdataUrl();
 
     @Nonnull
+    String userdataGrpcAddress();
+
+    default int userdataGrpcPort() {
+        return 8092;
+    }
+
+    @Nonnull
     String userdataJdbcUrl();
+
+    @Nonnull
+    String artistUrl();
+
+    @Nonnull
+    String artistGrpcAddress();
+
+    default int artistGrpcPort() {
+        return 8094;
+    }
+
+    @Nonnull
+    String artistJdbcUrl();
+
+    @Nonnull
+    String geoUrl();
+
+    @Nonnull
+    String geoGrpcAddress();
+
+    default int geoGrpcPort() {
+        return 8096;
+    }
+
+    @Nonnull
+    String geoJdbcUrl();
+
+    @Nonnull
+    String museumUrl();
+
+    @Nonnull
+    String museumGrpcAddress();
+
+    default int museumGrpcPort() {
+        return 8098;
+    }
+
+    @Nonnull
+    String museumJdbcUrl();
+
+    @Nonnull
+    String paintingUrl();
+
+    @Nonnull
+    String paintingGrpcAddress();
+
+    default int paintingGrpcPort() {
+        return 8100;
+    }
+
+    @Nonnull
+    String paintingJdbcUrl();
 }
--- rococo-e2e-tests/src/test/java/ru/rococo/config/LocalConfig.java
+++ rococo-e2e-tests/src/test/java/ru/rococo/config/LocalConfig.java
@@ -1,7 +1,5 @@
 package ru.rococo.config;
 
-import org.jetbrains.annotations.NotNull;
-
 import javax.annotation.Nonnull;
 
 public enum LocalConfig implements Config {
@@ -20,13 +18,13 @@ public enum LocalConfig implements Config {
         return "http://127.0.0.1:9000/";
     }
 
-    @NotNull
+    @Nonnull
     @Override
     public String gatewayUrl() {
         return "http://127.0.0.1:8080/";
     }
 
-    @NotNull
+    @Nonnull
     @Override
     public String userdataUrl() {
         return "http://127.0.0.1:8091/";
@@ -34,7 +32,85 @@ public enum LocalConfig implements Config {
 
     @Nonnull
     @Override
+    public String userdataGrpcAddress() {
+        return "127.0.0.1";
+    }
+
+    @Nonnull
+    @Override
     public String userdataJdbcUrl() {
         return "jdbc:postgresql://127.0.0.1:5432/rococo-userdata";
+    }
+
+    @Nonnull
+    @Override
+    public String artistUrl() {
+        return "http://127.0.0.1:8093/";
+    }
+
+    @Nonnull
+    @Override
+    public String artistGrpcAddress() {
+        return "127.0.0.1";
+    }
+
+    @Nonnull
+    @Override
+    public String artistJdbcUrl() {
+        return "jdbc:postgresql://127.0.0.1:5432/rococo-artist";
+    }
+
+    @Nonnull
+    @Override
+    public String geoUrl() {
+        return "http://127.0.0.1:8095/";
+    }
+
+    @Nonnull
+    @Override
+    public String geoGrpcAddress() {
+        return "127.0.0.1";
+    }
+
+    @Nonnull
+    @Override
+    public String geoJdbcUrl() {
+        return "jdbc:postgresql://127.0.0.1:5432/rococo-geo";
+    }
+
+    @Nonnull
+    @Override
+    public String museumUrl() {
+        return "http://127.0.0.1:8097/";
+    }
+
+    @Nonnull
+    @Override
+    public String museumGrpcAddress() {
+        return "127.0.0.1";
+    }
+
+    @Nonnull
+    @Override
+    public String museumJdbcUrl() {
+        return "jdbc:postgresql://127.0.0.1:5432/rococo-museum";
+    }
+
+    @Nonnull
+    @Override
+    public String paintingUrl() {
+        return "http://127.0.0.1:8099/";
+    }
+
+    @Nonnull
+    @Override
+    public String paintingGrpcAddress() {
+        return "127.0.0.1";
+    }
+
+    @Nonnull
+    @Override
+    public String paintingJdbcUrl() {
+        return "jdbc:postgresql://127.0.0.1:5432/rococo-painting";
     }
 }
